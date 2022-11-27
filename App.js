bcfd4d1558fbbe56074dcc63bb82da600bba4534
Divyanshi Vishwakarma <103261764+Divyanshi51@users.noreply.github.com>
2022-11-27 11:28:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-// import Accordation from "./Accordion/Accordion";
-// import Birthday from "./Birthdayreminder/Birthday";
-// import Context from "./Trials/Context";
-// import Liftingstateup from "./Trials/Liftingstateup";
-// import Propsdrilling from "./Trials/Propsdrilling";
-// import Reducer from "./Trials/Reducer";
-// import UseRef from './Trials/UseRef'
 import Todo from "./Todolist/Todo";
 function App() {
   return (
     <>
-    {/* <Birthday/> */}
-    {/* <UseRef/> */}
-    {/* <Propsdrilling/> */}
-    {/* <Liftingstateup/> */}
-    {/* <Reducer/> */}
-    {/* <Context/> */}
-    {/* <Accordation/> */}
     <Todo/>
     </>
   );
